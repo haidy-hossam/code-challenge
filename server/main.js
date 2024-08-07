ad958462f594b9f56887febe19046d5ff864ea0e
haidy-hossam <haidyhossam97@hotmail.com>
2024-08-07 23:54:59 +0300

    code-challenge

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { loadInitialData } from '../infra/initial-data';
+import { Communities } from '../communities/communities';
+import { People } from '../people/people';
+import { Counts } from '../counts/counts';
 
 Meteor.startup(async () => {
   // DON'T CHANGE THE NEXT LINE
@@ -7,3 +10,9 @@ Meteor.startup(async () => {
 
   // YOU CAN DO WHATEVER YOU WANT HERE
 });
+
+Meteor.publish('communities', () => Communities.find({}));
+
+Meteor.publish('people', (communityId) => People.find({ communityId }));
+
+Meteor.publish('counts', (communityId) => Counts.find({ communityId }));
